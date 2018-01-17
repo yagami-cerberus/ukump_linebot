65ab8e6c326b1ec918d13e77c6809d0130dd339b
yagami.cerberus <yagami-cerberus@users.noreply.github.com>
2018-01-17 21:54:33 +0800

    T

--- install.sh
+++ install.sh
@@ -15,35 +15,31 @@ fi
 
 echo 安裝必要系統套件...
 sleep 0.5
-sudo apt-get -y install uwsgi python3-pip git libpq-dev python3-dev uwsgi-plugin-python3 nodejs npm
+sudo apt-get -y install uwsgi python3-pip git libpq-dev python3-dev uwsgi-plugin-python3 node nodejs npm build-essential chrpath libssl-dev libxft-dev libfreetype6 libfreetype6-dev libfontconfig1 libfontconfig1-dev phantomjs
 if [ $? -ne 0 ]; then
     echo 無法安裝必要系統套件...
     exit 1
 fi
 
-echo "安裝 nodejs 相關套件(出png圖用)..."
-sleep 0.5
-npm install -g phantomjs
 
-
-echo 安裝 python 相關套件...
+echo "安裝 python 相關套件..."
 sleep 0.5
 pip3 install --upgrade pip
 pip3 install django psycopg2 line-bot-sdk python-dateutil
 
 
-echo 準備安裝 ukumpcore...
+echo "準備安裝 ukumpcore..."
 [ -d /var/repository ] && echo "/var/repository 已建立" || mkdir /var/repository
 
 
 cd /var/repository
-if [ -d ukumpcore ]; then
+if [ -d ukump_linebot ]; then
     echo 準備更新程式碼...
-    cd ukumpcore
+    cd ukump_linebot
     git pull
 else
     echo 下載程式碼...
-    git clone git@github.com:yagami-cerberus/ukump_linebot.git
+    git clone https://github.com/yagami-cerberus/ukump_linebot.git
 fi
 
 if [ $? -ne 0 ]; then
@@ -52,7 +48,13 @@ if [ $? -ne 0 ]; then
 fi
 
 
-echo 覆蓋 uwsgi 設定檔: /etc/uwsgi/apps-enabled/ukumpcore.xml
+echo "準備資料庫"
+sleep 0.5
+apt-get install postgresql
+sudo -u postgres createdb ukumpcore
+
+
+echo "覆蓋 uwsgi 設定檔: /etc/uwsgi/apps-enabled/ukumpcore.xml"
 sleep 0.5
 cat > /etc/uwsgi/apps-enabled/ukumpcore.xml <<- EOM
 <uwsgi>
@@ -61,7 +63,7 @@ cat > /etc/uwsgi/apps-enabled/ukumpcore.xml <<- EOM
 <socket>/tmp/ukumpcore_socket</socket>
 <chdir>/var/repository/ukumpcore</chdir>
 <module>ukumpcore.wsgi</module>
-<uid>www-data</uid>
+<uid>postgres</uid>
 <gid>www-data</gid>
 <master/>
 <processes>8</processes>
@@ -71,8 +73,7 @@ EOM
 
 ln -s /etc/uwsgi/apps-available/ukumpcore.xml /etc/uwsgi/apps-enabled/ukumpcore.xml
 
-
-echo 覆蓋 nginx ssl 設定檔: /etc/nginx/sites-enabled/ssl
+echo "覆蓋 nginx ssl 設定檔: /etc/nginx/sites-enabled/ssl"
 sleep 0.5
 cat > /etc/nginx/sites-enabled/ssl <<- EOM
 server {
@@ -99,6 +100,20 @@ server {
 EOM
 
 
-echo 安裝程序完成，請確認後續安裝程序
-echo "1. ukumpcore 設定"
-echo "2. 資料庫 migration"
+apt-get install postgresql
+
+echo "準備設定檔"
+cd /var/repository/ukump_linebot
+[ -f /var/repository/ukump_linebot/ukumpcore/settings.py ] && echo "設定檔已存在" || cp /var/repository/ukump_linebot/ukumpcore/settings.py.example /var/repository/ukump_linebot/ukumpcore/settings.py
+sudo -u postgres ./manage.py createcachetable
+if [ $? -ne 0 ]; then
+    echo "建立快取資料表時發生錯誤"
+    exit 1
+fi
+
+sudo -u postgres ./manage.py migrate
+if [ $? -ne 0 ]; then
+    echo "Migrate 資料表時發生錯誤"
+    exit 1
+fi
+
